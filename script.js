2426c9afd97f5ca4b49508401e7398f761be141e
Code Sandbox <hello@codesandbox.io>
2023-10-06 14:48:16 +0000

    Deployment from Codesandbox.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// query selector finds an element based on the element name, class name, or id
 const themeChangeButton = document.querySelector("#theme-change-button");
 
+// whenever the theme change button is clicked,
+// we check what the current data-theme is and toggle it to the opposite theme
 themeChangeButton.addEventListener("click", () => {
   const htmlElement = document.querySelector("html");
   const currentTheme = htmlElement.getAttribute("data-theme");
